9fb3d3e07544aef239c842761bdca1c916bd95d6
Gavin Davis <gavin.davis37@hotmail.com>
2023-01-04 23:19:33 +0000

    Updated server, added watchtower script

--- src/main/java/javalinproject/Server.java
+++ src/main/java/javalinproject/Server.java
@@ -6,7 +6,7 @@ public class Server {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Javalin app = Javalin.create(/*config*/)
-            .get("/", ctx -> ctx.result("Hello World"))
+            .get("/", ctx -> ctx.result("Goodbye World"))
             .start(7070);	    
 	}
 
